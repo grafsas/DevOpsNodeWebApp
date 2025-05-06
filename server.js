da0f99025974f4643d79451e9b4dd65f013dee69
grafsas <grafsas@students.zhaw.ch>
2025-05-06 14:44:03 +0200

    test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Sascha willkommen zur heutigen DevOps Vorlesung!!');
+  res.send('Hello Sascha willkommen zur heutigen DevOps Vorlesung!!!');
 });
 
 app.listen(PORT, HOST);
