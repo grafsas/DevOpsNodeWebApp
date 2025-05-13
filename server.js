2f86f310e50688813fd9d5b1cf6e040f3bf89ba4
grafsas <grafsas@students.zhaw.ch>
2025-05-13 15:01:59 +0200

    test commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Sascha willkommen zur heutigen DevOps Vorlesung!!! Funktioniet es jetzt endlich?!');
+  res.send('Hello Sascha willkommen zur heutigen DevOps Vorlesung!!! Docker und azure! GitHub acitons');
 });
 
 app.listen(PORT, HOST);
